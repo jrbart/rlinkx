b7cb7094ff260a653133db02f15bdd7696870902
jrbart <jorabart@gmail.com>
2025-06-15 06:20:02 -0500

    enable observer

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Rlinkx.MixProject do
   def application do
     [
       mod: {Rlinkx.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :observer, :wx]
     ]
   end
 
